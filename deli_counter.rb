e32dcd30c20f1bb867b75494950d22cc86e840e6
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-11 17:43:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def line(array)
+  
+  if array.length >= 1 
+    method_array = []
+    index = 1 
+    array.each do |name|
+      method_array.push("#{index}. #{name}")
+      index += 1 
+    end
+    puts "The line is currently: #{method_array.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+    
+end
+
+def take_a_number(array, name)
+end
